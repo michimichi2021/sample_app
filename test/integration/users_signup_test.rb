53cac3552c17990c667a7bddc8ed74dbf0ac2b18
EC2 Default User <michikaho1009@gmail.com>
2021-07-21 14:10:17 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,14 +7,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do
     
-    get signup_path
+     get signup_path
      assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+     post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
-    end
-    assert_template 'users/new'
+     end
+     assert_template 'users/new'
    
     
     
@@ -32,9 +32,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
+    
   end
   
   
